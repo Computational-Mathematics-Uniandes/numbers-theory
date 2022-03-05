9add187a8a8ef157589a3907715d8d5b6d1c88f9
Ratabart666 <88800858+Ratabart666@users.noreply.github.com>
2022-03-05 17:06:17 -0500

    Add files via upload

---
+++ algoritmo de euclides.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  6 19:28:27 2021
+
+@author: Asus
+"""
+
+def algoritmo_euclides(x:int,y:int)->int:
+    a=max(x,y)
+    b=min(x,y)
+    #El proceso iterativo es encontrar un entero e y un natural r, tal que 0=<r<e
+    #Estos enteros deben satisfacer que a=b*e+r
+    r=a%b#residuo
+    print(str(a)+str('=')+str(b)+str('*')+str(a//b)+str('+')+str(r))
+    if r!=0:
+        #Si el residuo no es cero se continua el proceso iterativo
+        return algoritmo_euclides(b,r)
+    else:
+        #cuando el residuo es cero se retorna b, que sería el MCD de (x,y)
+        return b
+print(algoritmo_euclides(54,1420))
+
+        
+        
+        
