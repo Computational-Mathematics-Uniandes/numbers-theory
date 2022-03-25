c33d59ed27a00ae66dd9fcbfbeb2f0c6d6cf0222
Ratabart666 <88800858+Ratabart666@users.noreply.github.com>
2022-03-25 12:34:10 -0500

    Update algoritmo de euclides.py

--- algoritmo de euclides.py
+++ algoritmo de euclides.py
@@ -4,7 +4,7 @@ def algoritmo_euclides(x:int,y:int)->int:
     #El proceso iterativo es encontrar un entero e y un natural r, tal que 0=<r<e
     #Estos enteros deben satisfacer que a=b*e+r
     r=a%b#residuo
-    print(str(a)+str('=')+str(b)+str('*')+str(a//b)+str('+')+str(r))
+    #print(str(a)+str('=')+str(b)+str('*')+str(a//b)+str('+')+str(r))
     if r!=0:
         #Si el residuo no es cero se continua el proceso iterativo
         return algoritmo_euclides(b,r)
